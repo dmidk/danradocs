1b0972e1be71a24fbd58e059dbebd2aca8364408
Søren Borg Nielsen <11832812+sbnielsen@users.noreply.github.com>
2025-08-15 13:16:27 +0200

    Fix markdwon syntax for superscritps

--- danra-book/scripts/parameter_list.py
+++ danra-book/scripts/parameter_list.py
@@ -1,17 +1,19 @@
 from pathlib import Path
 import xarray as xr
 import os
+import re
 
 def _retireve_variable_description(ds):
     md_lines = [
     "| Variable Name | CF Standard Name | Description | Unit |",
     "|---------------|------------------|-------------|------|"
     ]
-    for var in ds_danra_sl.data_vars:
-        da = ds_danra_sl[var]
+    for var in ds.data_vars:
+        da = ds[var]
         standard_name = da.attrs.get("standard_name", "")
         long_name = da.attrs.get("long_name", "")
         units = da.attrs.get("units", "")
+        units = re.sub(r"\*\*(-?\d+)", lambda m: f"<sup>{m.group(1)}</sup>", units)
         md_lines.append(f"| {var} | {standard_name} | {long_name} | {units} |")
     return md_lines
 
